a707d702ca5f55f67c63382ff45da573b3b27704
LeanLescano <tatilescano11@gmail.com>
2020-09-19 12:01:01 -0300

    Alertas cuando falla el login, dialog alert Mi Cuenta

--- app/src/main/java/utn/frgp/tusi/tp3_grupo_7/MainActivity.java
+++ app/src/main/java/utn/frgp/tusi/tp3_grupo_7/MainActivity.java
@@ -6,16 +6,15 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText username;
-    private EditText password;
-    private Toast alertEmpty;
-    private Toast alertErrorUser;
+    private EditText username, password;
+    private Toast alertEmpty, alertErrorPass, alertErrorUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,30 +25,30 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.edt_contrasenia);
         alertEmpty = Toast.makeText(getApplicationContext(),"Ambos campos deben estar completos.",Toast.LENGTH_SHORT);
         alertErrorUser = Toast.makeText(getApplicationContext(), "Nombre de usuario no encontrado.", Toast.LENGTH_SHORT);
+        alertErrorPass = Toast.makeText(getApplicationContext(), "Nombre de usuario o contraseña incorrecta.", Toast.LENGTH_SHORT);
     }
 
     public void login(View view){
-
         if(username.getText().length() > 0 && password.getText().length() > 0){
             AdminSQLite admin = new AdminSQLite(this, "BaseDatosTp3", null, 1);
             SQLiteDatabase BasedeDatos = admin.getWritableDatabase();
 
             String Username = username.getText().toString();
             String Password = password.getText().toString();
-            Cursor fila = BasedeDatos.rawQuery("select nombre , contrasenia from usuarios where nombre ='"+Username +"'" , null);
+            Cursor fila = BasedeDatos.rawQuery("select nombre from usuarios where nombre ='"+ Username +"'" , null);
             if(fila.moveToFirst()){
-                Cursor fila2 = BasedeDatos.rawQuery("select nombre, contrasenia from usuarios where contrasenia ='" + Password+"'", null);
+                Cursor fila2 = BasedeDatos.rawQuery("select id from usuarios where contrasenia ='" + Password +"' and nombre = '"+Username+"'", null);
                 if(fila2.moveToFirst()){
                     BasedeDatos.close();
                     Intent menu = new Intent(this, MenuActivity.class);
+                    menu.putExtra("id_user", fila2.getString(0));
                     startActivity(menu);
+                }else{
+                    alertErrorPass.show();
                 }
+            }else{
+                alertErrorUser.show();
             }
-            //if para chequear usuario en BD
-                //Si existe chequear la contraseña
-                    //Ingresar en la app
-                //Si no coincide, mostrar Toast
-            //Si no existe, mostrar Toast (alertErrorUser)
         }else{
             if(!alertEmpty.getView().isShown()){
                 alertEmpty.show();
--- app/src/main/java/utn/frgp/tusi/tp3_grupo_7/MenuActivity.java
+++ app/src/main/java/utn/frgp/tusi/tp3_grupo_7/MenuActivity.java
@@ -1,14 +1,42 @@
 package utn.frgp.tusi.tp3_grupo_7;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button miCuenta;
+    private AlertDialog.Builder builder;
+    private AlertDialog dialogAccount;
+    private int idUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menuprincipal);
+        idUser = Integer.parseInt(getIntent().getStringExtra("id_user"));
+        miCuenta = (Button) findViewById(R.id.btn_profile);
+        builder = new AlertDialog.Builder(MenuActivity.this);
+        dialogAccount = builder.create();
+        dialogAccount.setTitle("Mi cuenta");
+
+        AdminSQLite admin = new AdminSQLite(this, "BaseDatosTp3", null, 1);
+        SQLiteDatabase BasedeDatos = admin.getWritableDatabase();
+        Cursor userData = BasedeDatos.rawQuery("select nombre, email from usuarios where id ="+ idUser , null);
+        if(userData.moveToFirst()){
+            dialogAccount.setMessage("Nombre: "+userData.getString(0)+" \nEmail: " + userData.getString(1));
+        }else{
+            dialogAccount.setMessage("Ha ocurrido un error.");
+        }
+    }
+
+    public void showAccount(View view){
+        dialogAccount.show();
     }
 }
