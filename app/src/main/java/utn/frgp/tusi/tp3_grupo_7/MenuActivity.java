ea45d4e7e0623bc3e24aa05588291b008bb72ebf
Juan <49132815+juanccassano@users.noreply.github.com>
2020-09-19 17:47:17 -0300

    Agregado el boton para cargar nuevo parqueo, el metodo y el dialog en la activity

--- app/src/main/java/utn/frgp/tusi/tp3_grupo_7/MenuActivity.java
+++ app/src/main/java/utn/frgp/tusi/tp3_grupo_7/MenuActivity.java
@@ -3,18 +3,24 @@ package utn.frgp.tusi.tp3_grupo_7;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
     private Button miCuenta;
     private AlertDialog.Builder builder;
-    private AlertDialog dialogAccount;
+    private AlertDialog.Builder builderP;
+    private AlertDialog dialogAccount, dialogParqueo;
     private int idUser;
+    private Toast alertEmpty, alertExito, alertError;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +29,38 @@ public class MenuActivity extends AppCompatActivity {
         idUser = Integer.parseInt(getIntent().getStringExtra("id_user"));
         miCuenta = (Button) findViewById(R.id.btn_profile);
         builder = new AlertDialog.Builder(MenuActivity.this);
+        builderP = new AlertDialog.Builder(MenuActivity.this);
+        alertEmpty = Toast.makeText(getApplicationContext(), "Debe completar todos los campos.", Toast.LENGTH_SHORT);
+        alertExito = Toast.makeText(getApplicationContext(), "Parqueo registrado exitosamente", Toast.LENGTH_SHORT);
+        alertError = Toast.makeText(getApplicationContext(), "Ha ocurrido un error al intentar registrar el parqueo.", Toast.LENGTH_LONG);
+
         dialogAccount = builder.create();
         dialogAccount.setTitle("Mi cuenta");
+
+
+        builderP.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //Cambiar parametros
+                if (cargarParqueo("a",1))
+                {
+                    alertExito.show();
+                }
+                else
+                {
+                    alertError.show();
+                }
+            }
+        });
+        builderP.setNegativeButton(R.string.cancelar, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+
+            }
+        });
+
+
+        dialogParqueo = builderP.create();
+        dialogParqueo.setTitle("Cargar parqueo");
+
 
         AdminSQLite admin = new AdminSQLite(this, "BaseDatosTp3", null, 1);
         SQLiteDatabase BasedeDatos = admin.getWritableDatabase();
@@ -34,9 +70,53 @@ public class MenuActivity extends AppCompatActivity {
         }else{
             dialogAccount.setMessage("Ha ocurrido un error.");
         }
+
+
+
     }
+
+
+    public boolean cargarParqueo(String matriculaCargada, int tiempoCargado) {
+        String matricula = matriculaCargada;
+        int tiempo = tiempoCargado;
+
+
+        if (!matricula.isEmpty() && tiempo > 0) {
+
+            AdminSQLite admin = new AdminSQLite(this, "BaseDatosTp3", null, 1);
+            SQLiteDatabase BaseDatos = admin.getWritableDatabase();
+            try {
+                ContentValues registro = new ContentValues();
+                registro.put("patente", matriculaCargada);
+                registro.put("tiempo", tiempoCargado);
+                registro.put("id_usuario", idUser);
+                BaseDatos.insert("parqueos", null, registro);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return false;
+            } finally {
+                BaseDatos.close();
+
+            }
+        }
+
+        return true;
+    }
+
+
+
+
+
+
+
+
 
     public void showAccount(View view){
         dialogAccount.show();
+    }
+
+    public void showParqueo(View view){
+        dialogParqueo.show();
     }
 }
